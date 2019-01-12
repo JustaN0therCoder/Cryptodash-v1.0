0a5935b95a899610163dfb1cc3c54e71a0a7d2ad
Jordan Dossett <jordandossett98@gmail.com>
2019-01-12 10:55:40 -0600

    Added Images and some other stuff

--- src/Settings/CoinGrid.js
+++ src/Settings/CoinGrid.js
@@ -2,6 +2,7 @@ import React from 'react';
 import styled,{css} from 'styled-components';
 import {AppContext} from "../App/AppProvider";
 import {SelectableTile} from '../Shared/Tile';
+import CoinTile from './CoinTile';
 
 export const CoinGridStyled = styled.div`
   display:grid;
@@ -10,10 +11,15 @@ export const CoinGridStyled = styled.div`
 
 `
 
+function getCoinsToDisplay(coinList) {
+  return Object.keys(coinList).slice(0,100);
+}
+
 export default function() {
   return <AppContext.Consumer>
   {({coinList}) => <CoinGridStyled>
-    {Object.keys(coinList).map(coinKey => <SelectableTile>{coinKey}</SelectableTile>)}
+    {getCoinsToDisplay(coinList).map(coinKey =>
+      <CoinTile coinKey = {coinKey}/>)}
   </CoinGridStyled>}
   </AppContext.Consumer>
 }
