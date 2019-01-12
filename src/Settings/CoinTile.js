9c336113678c0fcea7f27ca75c9b423c59fb7df4
Jordan Dossett <jordandossett98@gmail.com>
2019-01-12 11:09:25 -0600

    Styled the top section

--- src/Settings/CoinTile.js
+++ src/Settings/CoinTile.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import {AppContext} from '../App/AppProvider';
-import {SelectableTile} from '../Shared/Tile';
+import {SelectableTile, DisabledTile, DeletableTile} from '../Shared/Tile';
 import CoinHeaderGrid from './CoinHeaderGrid';
 import CoinImage from '../Shared/CoinImage';
-export default function({coinKey}){
+
+
+export default function({coinKey, topSection}){
 
   return(
     <AppContext.Consumer>
       {({coinList}) =>{
         let coin = coinList[coinKey];
-        const TitleClass = SelectableTile;
+        let TitleClass = SelectableTile;
+        if(topSection){
+          TitleClass = DeletableTile;
+        }
+
         return <TitleClass>
-        <CoinHeaderGrid name={coin.CoinName} symbol={coin.Symbol}/>
+        <CoinHeaderGrid topSection = {topSection} name={coin.CoinName} symbol={coin.Symbol}/>
         <CoinImage coin={coin}/>
         </TitleClass>
       }}
