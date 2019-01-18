369dbde5cf31d44af5d9ce372ffb95ba3a545231
Jordan Dossett <jordandossett98@gmail.com>
2019-01-18 08:35:48 -0600

    Added some comments

--- src/Dashboard/HighCharts.js
+++ src/Dashboard/HighCharts.js
@@ -31,6 +31,7 @@ export default function(){
       name: 'Installation',
       data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175]
     },
+    // These add more lines to the graph, Saving for future expirmentation
     // {
     //   name: 'Manufacturing',
     //   data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434]
